9bfa34d59c3ec4e1050b258ea927f0f2cedeac29
Thanee Ackermann <tackerm@arkin.nl>
2024-01-18 16:26:56 +0100

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,41 @@
+# Download data set from coursera
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "electricpowerconsumption.zip")
+
+# Unzip file
+unzip("electricpowerconsumption.zip")
+
+# Import data from txt file
+data <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", na.strings = "?")
+
+# Subset data from the dates 2007-02-01 and 2007-02-02
+data <- data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+# Convert "Date" and "Time" variables to "Date" and "Time" class
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+# Create a PNG file with a width of 480 pixels and a height of 480 pixels
+png("plot4.png", width = 480, height = 480)
+
+# Create 2x2 layout for plot4
+par(mfrow = c(2, 2))
+
+# Create the plots for plot4
+plot(data$Datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power", xaxt = "n")
+axis(side = 1, at = c(as.numeric(data$Datetime[1]), as.numeric(data$Datetime[1441]),as.numeric(data$Datetime[2880])), labels = c("Thu", "Fri", "Sat"))
+
+plot(data$Datetime, data$Voltage, xlab = "datetime", ylab = "Voltage", type = "l", xaxt = "n")
+axis(side = 1, at = c(as.numeric(data$Datetime[1]), as.numeric(data$Datetime[1441]),as.numeric(data$Datetime[2880])), labels = c("Thu", "Fri", "Sat"))
+
+plot(data$Datetime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering", xaxt = "n")
+lines(data$Datetime, data$Sub_metering_2, col = "red")
+lines(data$Datetime, data$Sub_metering_3, col = "blue")
+axis(side = 1, at = c(as.numeric(data$Datetime[1]), as.numeric(data$Datetime[1441]),as.numeric(data$Datetime[2880])), labels = c("Thu", "Fri", "Sat"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, cex = 0.8, bty = "n")
+
+plot(data$Datetime, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_Reactive-Power", xaxt = "n")
+axis(side = 1, at = c(as.numeric(data$Datetime[1]), as.numeric(data$Datetime[1441]),as.numeric(data$Datetime[2880])), labels = c("Thu", "Fri", "Sat"))
+
+# Save it to the PNG file and close device
+dev.off()
